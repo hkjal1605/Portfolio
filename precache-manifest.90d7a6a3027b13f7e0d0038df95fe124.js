3958b8fc9035e69f425136d187b798fe9ce96176
Harsh Kumar Jha <hkjal1605@gmail.com>
2020-11-22 10:26:55 +0530

    Updates

---
+++ precache-manifest.90d7a6a3027b13f7e0d0038df95fe124.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0bca36d22ba1bea86072ba9a151930f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9205520ad59a2c3e7da1",
+    "url": "/Portfolio/static/css/main.4450df58.chunk.css"
+  },
+  {
+    "revision": "3c8b4a2edb88c0102834",
+    "url": "/Portfolio/static/js/2.52b71921.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.52b71921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9205520ad59a2c3e7da1",
+    "url": "/Portfolio/static/js/main.802fb655.chunk.js"
+  },
+  {
+    "revision": "627bdb01d220a474fd8b",
+    "url": "/Portfolio/static/js/runtime-main.9e287cfb.js"
+  },
+  {
+    "revision": "6fe07119cf7719d3da1708e258e2698d",
+    "url": "/Portfolio/static/media/Capture.6fe07119.webp"
+  },
+  {
+    "revision": "5e8bd5806bee32d6e40656e3707339e3",
+    "url": "/Portfolio/static/media/Capture2.5e8bd580.webp"
+  },
+  {
+    "revision": "c19b209a816eac3635808879b01d97ca",
+    "url": "/Portfolio/static/media/Capture3.c19b209a.webp"
+  },
+  {
+    "revision": "3752cb81f3b76377955bd7d7caf36f8f",
+    "url": "/Portfolio/static/media/Capture6.3752cb81.webp"
+  },
+  {
+    "revision": "d11f53a44720c97f88cee98a28c0665c",
+    "url": "/Portfolio/static/media/bg.d11f53a4.webp"
+  },
+  {
+    "revision": "e056943df57ddf0e193c7391fcd09957",
+    "url": "/Portfolio/static/media/blob.e056943d.svg"
+  },
+  {
+    "revision": "57d3f125e2df9795fe8af82fcd70dc58",
+    "url": "/Portfolio/static/media/facebook.57d3f125.svg"
+  },
+  {
+    "revision": "7f8d489c1887554fcf61932505d46393",
+    "url": "/Portfolio/static/media/firebase-1.7f8d489c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/Portfolio/static/media/github-1.0ff1a6f0.svg"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/Portfolio/static/media/instagram.5b23a837.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/Portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "7ff65e9690ccb63d679d1153bdee612f",
+    "url": "/Portfolio/static/media/logo-algolia-square-dark.7ff65e96.svg"
+  },
+  {
+    "revision": "80e6f1db7ba281383f66bd584a340b64",
+    "url": "/Portfolio/static/media/me.80e6f1db.webp"
+  },
+  {
+    "revision": "4643494b26276d291b810d5610789aa7",
+    "url": "/Portfolio/static/media/mongodb.4643494b.svg"
+  },
+  {
+    "revision": "b337a2fb0b1289e6757e632dc766ff6b",
+    "url": "/Portfolio/static/media/nodejs-icon.b337a2fb.svg"
+  },
+  {
+    "revision": "e89c97dd4d68aef752d20813a81d94dc",
+    "url": "/Portfolio/static/media/previous.e89c97dd.svg"
+  },
+  {
+    "revision": "c3fbd71b12b726e00eec2b59f3c76786",
+    "url": "/Portfolio/static/media/react-2.c3fbd71b.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "/Portfolio/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "69cedcf3d2b148bdce953a480caa57b1",
+    "url": "/Portfolio/static/media/vue-js-1.69cedcf3.svg"
+  }
+]);
